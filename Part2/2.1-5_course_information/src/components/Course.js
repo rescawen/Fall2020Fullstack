fc82b6531cc004d9256b9513ecc776caa9c59128
Wenlei Dai <resca.wen@gmail.com>
2020-09-18 16:36:36 +0300

    cleaned up course module

--- Part2/2.1-5_course_information/src/components/Course.js
+++ Part2/2.1-5_course_information/src/components/Course.js
@@ -10,17 +10,19 @@ const Part = ({ part }) => {
 
 const Course = ({ course }) => {
 
+  const courseName = <h2>{course.name}</h2>
+  const courseContent = course.parts.map(part =>
+    <Part key={part.id} part={part} />
+  )
   const exercises = course.parts.map(part => part.exercises)
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  const totalExercises = exercises.reduce(reducer)
-  
+  const totalExercises = <b>total of {exercises.reduce(reducer)} exercises</b> 
+
   return (
     <div>
-      <h2>{course.name}</h2>
-      {course.parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-      <b>total of {totalExercises} exercises</b>
+      {courseName}
+      {courseContent}
+      {totalExercises}
     </div>
   )
 }
