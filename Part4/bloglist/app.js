717e7ba6e55a683f378b26b13579278d70022a76
Wenlei Dai <resca.wen@gmail.com>
2020-10-21 20:01:56 +0300

    potentially fixed URI being exposed due to logging

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -7,16 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-// const blogSchema = new mongoose.Schema({
-//     title: String,
-//     author: String,
-//     url: String,
-//     likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-logger.info('connecting to', config.MONGODB_URI)
+// logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => {
@@ -31,25 +22,6 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
-
-
-// app.get('/api/blogs', (request, response) => {
-//     Blog
-//         .find({})
-//         .then(blogs => {
-//             response.json(blogs)
-//         })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//     const blog = new Blog(request.body)
-
-//     blog
-//         .save()
-//         .then(result => {
-//             response.status(201).json(result)
-//         })
-// })
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
